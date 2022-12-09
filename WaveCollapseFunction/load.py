3038bfecae7cbf75ee0afe3d40de8f156bf02463
markspark69420 <95381754+markspark69420@users.noreply.github.com>
2022-12-09 13:11:25 +0000

    zadania

--- WaveCollapseFunction/load.py
+++ WaveCollapseFunction/load.py
@@ -1,5 +1,18 @@
+import os
 from PIL import Image
 
-i = Image.open(".\\res\\water-path\\path.png")
-
-print(i.size)
+def load(folder):
+    imgs = []
+    read = {}
+    for i in os.listdir(folder):
+        if i.endswith(".txt"):
+            pass
+        else:
+            imgs.append(Image.open(folder+'/'+i))
+    return read, imgs
+def run():
+    check = 3
+    l
+    for i in 
+if __name__ == "__main__":
+    print(load("./res/pipes-code-train")[1])
